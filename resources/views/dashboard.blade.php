81868880a4045e81ca65bcbacdd9d12d08e78201
SunnyTechie <sunnyict001@gmail.com>
2023-03-07 17:05:03 +0100

    UI completed 80%

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,9 +3,62 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-12 justify-center-center">
-                Hello world.
+            <div class="col-md-12 mt-5">
+                <div class="grid">
+                    <div class="grid-item"><a href="#"><img src="https://placehold.co/600x600"></a></div>
+                    <div class="grid-item"><a href="#"><img src="https://placehold.co/600x400"></a></div>
+                    <div class="grid-item"><a href="#"><img src="https://placehold.co/800x400"></a></div>
+                    <div class="grid-item"><a href="#"><img src="https://placehold.co/1200x1000"></a></div>
+                    <div class="grid-item"><a href="#"><img src="https://placehold.co/1920x1080"></a></div>
+                    <div class="grid-item"><a href="#"><img src="https://placehold.co/1000x2000"></a></div>
+                    <div class="grid-item"><a href="#"><img src="https://placehold.co/1600x1400"></a></div>
+                    <div class="grid-item"><a href="#"><img src="https://placehold.co/1000x1000"></a></div>
+                    <div class="grid-item"><a href="#"><img src="https://placehold.co/1800x2000"></a></div>
+                    <div class="grid-item"><a href="#"><img src="https://placehold.co/2000x1500"></a></div>
+                    <div class="grid-item"><a href="#"><img src="https://placehold.co/3000x4000"></a></div>
+                    <div class="grid-item"><a href="#"><img src="https://placehold.co/1350x1000"></a></div>
+                </div>
             </div>
         </div>
     </div>
+
+    <!-- Modal -->
+    <div class="modal fade" tabindex="-1" id="uploadModal">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Upload Media Files</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+             <form action="#" method="POST" class="dropzone" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" id="title" name="title" required>
+                </div>
+                <div class="form-group">
+                    <label for="pictures">Pictures</label>
+                    <div class="dropzone" id="myDropzone"></div>
+                </div>
+             </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary border-0" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary border-0" style="background: #14BC7D">Publish</button>
+            </div>
+          </div>
+        </div>
+    </div>
+
+      <script>
+        $(document).ready(function () {
+            Dropzone.autoDiscover = false;
+            var myDropzone = new Dropzone("#myDropzone", {
+                url: "#",
+                maxFiles: 10,
+                acceptedFiles: "image/*",
+            });
+            });
+      </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,12 +22,53 @@
 
         {{-- favicon --}}
         <link rel="icon" href="https://sfiloveinaction.org/wp-content/uploads/2022/07/cropped-SFI-Logo.png">
-        
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
   
         <!-- Bootstrap Icons -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.7.2/bootstrap-icons.min.css">
+        {{-- Dropzone --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.2/dropzone.min.css" integrity="sha512-v6U9bYjCXz+TppwJhLBy6SRRJ8/od0Kyv1bbp0x+oINP8G02WgBv7jK52yOFOf2r75dR1aSl2l5oZ+C5jVmyeA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Style+Script&display=swap');
 
+            body, p, li, a, ul, div, span, section, button, h1, h2, h3, h4, h5{
+                font-family: 'Poppins', sans-serif;
+                font-size: 14px;
+                font-weight: 400px;
+            }
+
+            .navbar-brand {
+                font-family: 'Style Script', cursive;
+                font-size: 30px;
+            }
+            .category-btn:active, .category-btn:focus, .category-btn:hover {
+                    background:#F7F7F7 !important; border-radius: 100px; box-shadow: none !important; color: #000 !important;
+                }
+
+                .category-btn {
+                    border-radius: 100px;
+                }
+                .nav-link-upload {
+                    color: #FFFFFF !important;
+                }
+
+                .grid {
+                    columns: 4;
+                    column-gap: 15px;
+                }
+
+                .grid-item {
+                    margin-bottom: 15px;
+                    break-inside: avoid;
+                }
+
+                .grid-item img {
+                    width: 100%;
+                    height: auto;
+                    display: block;
+                }
+        </style>
     </head>
     <body>
    
@@ -43,14 +84,14 @@
                   <li class="nav-item">
                     <a class="nav-link" href="#" style="color: #fff">Explore</a>
                   </li>
-                  <li class="nav-item">
+                  <li class="nav-item mx-2">
                     <a class="nav-link" href="#" style="color: #fff">Login</a>
                   </li>
-                  <li class="nav-item">
+                  <li class="nav-item mx-2">
                     <a class="nav-link" href="#" style="color: #fff">Join</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#" style="color: #fff">Upload</a>
+                    <button type="button" class="nav-link nav-link-upload border-0 btn px-3 ms-3" data-bs-toggle="modal" data-bs-target="#uploadModal" style="color: #fff; background: #14BC7D; border-radius: 50px">Upload</button>
                   </li>
                 </ul>
               </div>
@@ -59,28 +100,33 @@
 		<!-- /NAVIGATION -->
 
         {{-- Search Section --}}
-        <div class="header" style="background-image: url('https://cdn.pixabay.com/photo/2018/01/24/19/49/people-3104635_960_720.jpg'); height: 70vh; background-position: center; background-size: cover;">
+        <div class="header" style="background-image: url('https://cdn.pixabay.com/photo/2018/01/24/19/49/people-3104635_960_720.jpg'); height: 68vh; background-position: center; background-repeat: no-repeat; background-size: cover; opacity: 1; background-color: rgba(0, 0, 0, 0.5);">
             <div class="container">
-                <div class="spacer" style="height: 45vh"></div>
-                <form class="d-flex justify-content-center align-items-center">
-                    <div class="input-group">
-                        <div>Searchable Icon</div>
-                      <input type="text" class="form-control" placeholder="Search" aria-label="Search">
-                      <select class="form-select me-2" aria-label="Category">
-                        <option selected>Category</option>
-                        <option value="1">Option 1</option>
-                        <option value="2">Option 2</option>
-                        <option value="3">Option 3</option>
-                      </select>                     
+                <div class="row">
+                    <div class="col-md-8 offset-md-2">
+                        <div class="spacer" style="height: 45vh"></div>
+                        <form class="d-flex py-2 justify-content-center align-items-center" style="background: #FFFFFF; border-radius: 100px">
+                            <button class="border-0" type="submit" style="background: #FFF; margin-top: 4px"><ion-icon name="search"></ion-icon></button>
+                            <input type="text" class="form-control border-0" placeholder="Search" aria-label="Search" style="width: 70%; box-shadow: none;">
+                            <select class="form-select btn btn-light category-btn border-0 me-2" aria-label="Category" style="width: 20%;">
+                                <option selected>Category</option>
+                                <option value="1">Option 1</option>
+                                <option value="2">Option 2</option>
+                                <option value="3">Option 3</option>
+                            </select>                     
+                            
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
-          </div>
+        </div>
         {{-- End Search Section --}}
-            <main>
-                @yield('content')
-            </main>
-            <section style="border-top: 0.1rem solid #ddd; padding: 50px 0; margin-top: 40px; min-height: 50vh">
+        <main>
+            @yield('content')
+        </main>
+            
+            
+        <section style="border-top: 0.1rem solid #ddd; padding: 50px 0; margin-top: 40px; min-height: 50vh">
             <div class="container">
                 <div class="row">
                     <div class="col-md-7">
@@ -118,10 +164,11 @@
                     </div>
                 </div>
             </div>
-            </section>
-            <footer style="background: #F7F7F7; padding: 20px">
-                <p class="text-center text-black">This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.</p>
-            </footer>
+        </section>
+        <footer style="background: #F7F7F7; padding: 20px">
+            <p class="text-center text-black">This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.</p>
+        </footer>
+
         <!-- Ionicons -->
         <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
@@ -129,6 +176,9 @@
         <!-- jQuery Plugins -->
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+        {{-- Dropzone --}}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.2/min/dropzone.min.js" integrity="sha512-CWkSZewynvZfjKZCE9iee8WlGJnVn0f1cQRrQ6KPKU6ihG/l6N1Z6DF9U6bKTTsc0vTJw/0oUCPzKmz0o3OeOQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
         <script>
             $(document).ready(function() {
